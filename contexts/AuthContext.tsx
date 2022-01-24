332b9cf3a8df68bf5f845308ff3151d334bbf910
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-01-24 16:57:06 -0300

    salvando os tokens no cokkies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { setCookie } from "nookies";
 import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
 
@@ -40,7 +41,14 @@ export function AuthProvider({children}: AuthProviderProps ){
 
       const {token,  refreshToken ,permissions , roles } = await response.data
 
-      
+      setCookie(undefined, 'nextauth.token', token,{
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken,{
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
       
       setUser({
         email,
